251a4e950ae5bbaac815a99b2d591301e0294584
Chelsea Thompson <chelseathompson001@gmail.com>
2023-08-27 14:45:56 -0400

    added code to reviews.py and added csv file

--- reviews.py
+++ reviews.py
@@ -1,2 +1,11 @@
-# add your code here
-
+import pandas as pd
+# read csv file
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip", index_col=0)
+# unique countries and number of reviews
+uniq_country_reviews = reviews['country'].value_counts()
+# average points for unique countries rounded to 1 decimal point
+average_points = reviews.groupby('country')['points'].mean().round(1)
+# summary dataframe
+summary_df = pd.DataFrame.merge(uniq_country_reviews, average_points, on='country', how='inner')
+# writing the data to csv
+summary_df.to_csv("data/reviews-per-country.csv")
